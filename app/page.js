b8c67289660abbac444a609919e87f40713b99eb
Clue355 <dm.carlosluevano@gmail.com>
2024-02-05 17:46:29 -0600

    edit: home component

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Hello Shashi</h1>
-            <h2>something</h2>
+            <h2></h2>
         </main>
     );
 }
