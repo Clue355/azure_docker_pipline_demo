a96424b0323c9000e4d5a9f7182e18dd948b9cd6
Clue355 <dm.carlosluevano@gmail.com>
2024-02-05 20:48:44 -0600

    edit: home component

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Hello Shashi</h1>
-            <h2></h2>
+            <h2>Test to see if new code is deployed. If you see this it was a success</h2>
         </main>
     );
 }
